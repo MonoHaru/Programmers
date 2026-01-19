cd4b46367479c646519a1691dd016bf6f6c966e0
MonoHaru <72691118+MonoHaru@users.noreply.github.com>
2026-01-19 13:21:20 +0900

    feat: Update 글자 이어 붙여 문자열 만들기 문제 풀이

---
+++ LV0/글자이어붙여문자열만들기.py
@@ -0,0 +1,10 @@
+# LV0 - 글자 이어 붙여 문자열 만들기
+def solution(my_string, index_list):
+    """
+    :param my_string: 문자열
+    :param index_list: 정수 배열
+    """
+    return ''.join([my_string[i] for i in index_list])
+
+print(solution("cvsgiorszzzmrpaqpe", [16, 6, 5, 3, 12, 14, 11, 11, 17, 12, 7]))  # "programmers"
+print(solution("zpiaz", [1, 2, 0, 0, 3]))  # "pizza"
