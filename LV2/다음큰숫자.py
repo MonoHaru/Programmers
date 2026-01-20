e6952100aa954d8e0fdfc68a43eeefd559f186b2
MonoHaru <72691118+MonoHaru@users.noreply.github.com>
2026-01-20 20:21:03 +0900

    feat: Update 다음 큰 숫자 문제 풀이

---
+++ LV2/다음큰숫자.py
@@ -0,0 +1,15 @@
+# LV2 - 다음 큰 숫자 문제의 해답
+def solution(n):
+    """
+    :param n: int -> 자연수
+    
+    :retrun: n의 2진수에서 '1' 갯수가 같은 n보다 큰 수
+    """
+    bin_n = bin(n)[2:].count('1')
+
+    while True:
+        n += 1
+        if bin_n ==bin(n)[2:].count('1'):
+            break
+        
+    return n
