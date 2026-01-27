c96b6f04cf45db828eb285576cb37d4f3586c608
MonoHaru <72691118+MonoHaru@users.noreply.github.com>
2026-01-27 16:23:40 +0900

    feat: Add 카펫 문제 풀이

---
+++ LV2/카펫.py
@@ -0,0 +1,12 @@
+# LV2 - 카펫 문제의 해답
+def solution(brown, yellow):
+    for i in range(1, yellow + 1):
+        if yellow % i == 0:
+            x = i + 2
+            y = (yellow // i) + 2
+            if x * y == brown + yellow:
+                return [max(x, y), min(x, y)]
+            
+print(solution(10, 2))  # [4, 3]
+print(solution(8, 1))  # [3, 3]
+print(solution(24, 24))  # [8, 6]
