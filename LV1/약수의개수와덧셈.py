eaceaa3af31f39310333e5e506e6ddb3d12af332
MonoHaru <72691118+MonoHaru@users.noreply.github.com>
2026-01-27 17:41:29 +0900

    feat: Add 구명보트 문제 풀이

--- LV1/약수의개수와덧셈.py
+++ LV1/약수의개수와덧셈.py
@@ -1,5 +1,10 @@
 # LV1 - 약수의 개수와 덧셈 문제의 해답
 def solution(left, right):
+    """
+    :param left: int
+    :param right: int
+    :return: int
+    """
     return sum([-i if i**0.5 == int(i**0.5) else i for i in range(left, right + 1)])
     
 
---
+++ LV2/구명보트.py
@@ -0,0 +1,21 @@
+# LV2 - 구명보트 문제의 해답
+def solution(people, limit):
+    """
+    :param people: list
+    :param limit: int
+    :return: int
+    """
+    people.sort()
+    
+    cnt = 0
+    light = 0
+    heavy = len(people) - 1
+    while light < heavy:
+        if people[light] + people[heavy] <= limit:
+            light += 1
+            cnt += 1
+        heavy -= 1
+        
+        if light == heavy:
+            break
+    return len(people) - cnt
