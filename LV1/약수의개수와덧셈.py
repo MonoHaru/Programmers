cf8d150c5b42b1cfea30e8b3564255e739d78741
MonoHaru <72691118+MonoHaru@users.noreply.github.com>
2026-01-27 17:24:27 +0900

    feat: 약수의 개수와 덧셈 문제 풀이

---
+++ LV1/약수의개수와덧셈.py
@@ -0,0 +1,7 @@
+# LV1 - 약수의 개수와 덧셈 문제의 해답
+def solution(left, right):
+    return sum([-i if i**0.5 == int(i**0.5) else i for i in range(left, right + 1)])
+    
+
+print(solution(13, 17))  # 43
+print(solution(24, 27))  # 52
