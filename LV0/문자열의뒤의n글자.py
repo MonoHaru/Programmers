6ce9974e88ffffca6262a1606ddf31e6d1cfe222
MonoHaru <72691118+MonoHaru@users.noreply.github.com>
2026-01-27 17:01:54 +0900

    feat: Add 문자열의 뒤의 n글자 문제 풀이

---
+++ LV0/문자열의뒤의n글자.py
@@ -0,0 +1,10 @@
+# LV0 - 문자의 뒤의 n글자
+def solution(my_string, n):
+    """
+    :param my_string: str
+    :param n: int
+    """
+    return my_string[-n:]
+
+print(solution("ProgrammerS123", 11))  # "grammerS123"
+print(solution("He110W0r1d", 5))  # "W0r1d"
