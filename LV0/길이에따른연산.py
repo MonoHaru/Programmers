b49a2d75db028bc02dd2d8b5cd792a9f1ac95093
MonoHaru <72691118+MonoHaru@users.noreply.github.com>
2026-04-14 23:01:26 +0900

    feat: Add 길이에 따른

---
+++ LV0/길이에따른연산.py
@@ -0,0 +1,10 @@
+def solution(num_list):
+    """
+    :param num_list: list
+    :return: int
+    """
+    from math import prod
+    return sum(num_list) if len(num_list) >= 11 else prod(num_list)
+
+print(solution([3, 4, 5, 2, 5, 4, 6, 7, 3, 7, 2, 2, 1]))  # 51
+print(solution([2, 3, 4, 5]))  # 120
