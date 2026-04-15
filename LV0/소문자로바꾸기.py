e045d5cf33a5b3074ac144f26fdfebaf457ea3dc
MonoHaru <72691118+MonoHaru@users.noreply.github.com>
2026-04-15 22:22:32 +0900

    feat: Add 소문자로 바꾸기 문제의 해답

---
+++ LV0/소문자로바꾸기.py
@@ -0,0 +1,10 @@
+# LV0 - 소문자로 바꾸기 문제의 해답
+def solution(myString):
+    """
+    :param myString: str
+    :return: str
+    """
+    return myString.lower()
+
+print(solution("aBcDeFg"))  # "abcdefg"
+print(solution("aaa"))  # "aaa"
