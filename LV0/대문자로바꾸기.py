2ff81c505191c7eeecb7583a4fc5993bad43fb31
MonoHaru <72691118+MonoHaru@users.noreply.github.com>
2026-04-15 22:21:10 +0900

    feat: Add 대문자로 바꾸기 문제의 해답

---
+++ LV0/대문자로바꾸기.py
@@ -0,0 +1,10 @@
+# LV0 - 대문자로 바꾸기 문제의 해답
+def solution(myString):
+    """
+    :param myString: str
+    :return: str
+    """
+    return myString.upper()
+
+print(solution("aBcDeFg"))  # "ABCDEFG"
+print(solution("AAA"))  # "AAA"
