a0f0ff881ffa90f7ce42f296cbb106ab958ecae9
MonoHaru <72691118+MonoHaru@users.noreply.github.com>
2026-05-02 22:32:44 +0900

    feat: Add 간단한 식 계산하기 문제의 해답

---
+++ LV0/간단한식계산하기.py
@@ -0,0 +1,25 @@
+# LV0 - 간단한 식 계산하기 문제의 해답
+def solution(binomial: str) -> list:
+    """
+    간단한 식을 계산합니다.
+
+    Args:
+        binomial (str): 입력 문자열.
+    
+    Returns:
+        int: 문자열 계산식 결과. 
+    """
+    a, op, b = binomial.split(' ')
+    
+    a, b = int(a), int(b)
+    
+    if op == '+':
+        return a + b
+    elif op == '-':
+        return a - b
+    elif op == '*':
+        return a * b
+    
+print(solution("43 + 12"))  # 55
+print(solution("0 - 7777"))  # -7777
+print(solution("40000 * 40000"))  # 1600000000
