84df644e91bd2c06ce60d4ad1e0a5590d7b7eaf0
MonoHaru <72691118+MonoHaru@users.noreply.github.com>
2026-05-03 12:49:24 +0900

    feat: 세 개의 구분자 문자의 해답

--- LV0/rny_string.py
+++ LV0/rny_string.py
@@ -1,7 +1,7 @@
 # LV0 - rny_string 문제의 해답
 def solution(rny_string: str) -> str:
     """
-    rny_string에서 'm'을 'rn'로 교체한 문자열을 반환합니다.
+    rny_string에서 'm'을 'rn'으로 교체한 문자열을 반환합니다.
 
     Args:
         rny_string (str): 입력 문자열.
---
+++ LV0/세개의구분자.py
@@ -0,0 +1,23 @@
+# LV0 - 세 개의 구분자
+def solution(myStr: str) -> list:
+    """
+    myStr에서 'a', 'b', 그리고 'c' 기준으로 분할한 결과를 반환합니다.
+    분할 결과가 비어 있는 경우 ['EMPTY']를 반환합니다.
+
+    Args:
+        myStr (str): 입력 문자열.
+
+    Returns:
+        list: 분할된 문자열 리스트 또는 ['EMPTY'].
+    
+    """
+    import re
+    myStr = [x for x in re.split('[abc]', myStr) if x] 
+    if myStr:
+        return myStr
+    else:
+        return ['EMPTY']
+    
+print(solution("baconlettucetomato"))  # ["onlettu", "etom", "to"]
+print(solution("abcd"))  # ["d"]
+print(solution("cabab"))  # ["EMPTY"]
