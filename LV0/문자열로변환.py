802269da24590e8ec2da3dd9d05ae79a1f76698a
MonoHaru <72691118+MonoHaru@users.noreply.github.com>
2026-05-23 23:18:44 +0900

    feat: Add 문자열로 변환 문제의 해답

---
+++ LV0/문자열로변환.py
@@ -0,0 +1,15 @@
+# LV0 - 문자열로 변환 문제의 해답
+def solution(n: int) -> str:
+    """주어진 정수 n을 문자열로 변환합니다.
+
+    Args:
+        n (int): 주어진 정수.
+    
+    Returns:
+        str: 정수 n을 문자열로 변환합니다.
+    
+    """
+    return str(n)
+
+print(solution(123))  # "123"
+print(solution(2573))  # "2573"
