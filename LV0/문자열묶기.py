207aeeadab4885b5c3d3666ae78a24c1a102e338
MonoHaru <72691118+MonoHaru@users.noreply.github.com>
2026-05-27 21:18:54 +0900

    feat: Add 문자열 묶기 문제의 해답

--- LV0/문자열묶기.py
+++ LV0/문자열묶기.py
@@ -1,8 +1,16 @@
 # LV0 - 문자열 묶기 문제의 해답
-def solution(strArr: list[str]) -> int:
-    answer = dict()
-    for s in strArr:
-        answer[len(s)] = answer.get(len(s), 0) + 1
-    return max(answer.values())
+def solution(strArr: list) -> int:
+    """리스트에서 가장 많은 빈도의 문자열 길이를 반환합니다.
+
+    Args:
+        strArr (list[str]): 주어진 문자열 리스트.
+
+    Returns:
+        int: 최다 빈도수 문자열 길이.    
+    """
+    count = [0] * 31
+    for word in strArr:
+        count[len(word)] += 1
+    return max(count)
 
 print(solution(["a","bc","d","efg","hi"]))  # 2
