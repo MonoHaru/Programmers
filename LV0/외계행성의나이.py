fd6be6ce5d1c48c0c4dd42ba73b2fa0411c14632
MonoHaru <72691118+MonoHaru@users.noreply.github.com>
2026-05-28 20:14:57 +0900

    feat: Add 외계행성의 나이 문제의 해답

---
+++ LV0/외계행성의나이.py
@@ -0,0 +1,20 @@
+# LV0 - 외계행성의 나이
+def solution(age: int) -> str:
+    """정수 나이(age)를 문자로 표현합니다.
+
+    Args:
+        age (int): 정수 나이.
+
+    Returns:
+        str: 문자 나이.    
+    """
+    convert = {
+        '0': 'a', '1': 'b', '2': 'c', '3': 'd',
+        '4': 'e', '5': 'f', '6': 'g', '7': 'h',
+        '8': 'i', '9': 'j'
+    }
+    return ''.join([convert[w] for w in str(age)])
+
+print(solution(23))   # "cd"
+print(solution(51))	  # "fb"
+print(solution(100))  # "baa"
